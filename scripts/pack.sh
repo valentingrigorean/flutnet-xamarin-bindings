a86722309a1bf765ad9b129433020d509050e90d
Valentin Grigorean <valentin.grigorean1@gmail.com>
2022-08-18 10:01:31 +0300

    Remove Flutnet.interop.Java8

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -10,4 +10,4 @@ NUGET_EXE=$TOOLS_DIR/nuget/nuget.exe
 # Create NuGet packages for binding libraries
 mono "$NUGET_EXE" pack "$SRC_DIR/Flutnet.Interop.Android/Flutnet.Interop.Android.nuspec" -OutputDirectory "$ARTIFACTS_DIR/nuget-packages"
 mono "$NUGET_EXE" pack "$SRC_DIR/Flutnet.Interop.iOS/Flutnet.Interop.iOS.nuspec" -OutputDirectory "$ARTIFACTS_DIR/nuget-packages"
-mono "$NUGET_EXE" pack "$SRC_DIR/Flutnet.Interop.Java8/Flutnet.Interop.Java8.nuspec" -OutputDirectory "$ARTIFACTS_DIR/nuget-packages"
+#mono "$NUGET_EXE" pack "$SRC_DIR/Flutnet.Interop.Java8/Flutnet.Interop.Java8.nuspec" -OutputDirectory "$ARTIFACTS_DIR/nuget-packages"
