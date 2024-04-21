bf5b082255f0b02f91d0434caba92e7dbf60d855
Valentin Grigorean <valentin.grigorean1@gmail.com>
2024-04-21 17:04:04 +0300

    Releaes 3.19.6

--- scripts/build-src.sh
+++ scripts/build-src.sh
@@ -2,22 +2,19 @@
 
 # Define paths
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-SRC_DIR=$SCRIPT_DIR/../src
-VSTOOL_PATH="/Applications/Visual Studio.app/Contents/MacOS/vstool"
-MSBUILD_PATH=/Library/Frameworks/Mono.framework/Versions/Current/bin/msbuild
 SOLUTION_PATH=$SCRIPT_DIR/../Flutnet.Interop.sln
 
-# Restore Nuget Packages
-# The following command does NOT work for Xamarin projects
-# https://xamarin.github.io/bugzilla-archives/58/58254/bug.html
-# dotnet restore "$SOLUTION_PATH"
-"$MSBUILD_PATH" /t:restore "$SOLUTION_PATH"
+# Restore NuGet packages
+dotnet restore "$SOLUTION_PATH"
 
-# Clean and build binding libraries for all the available configurations
-"$VSTOOL_PATH" build --configuration:Debug --target:Clean "$SOLUTION_PATH"
-"$VSTOOL_PATH" build --configuration:ReleaseWithDebugNativeRef --target:Clean "$SOLUTION_PATH"
-"$VSTOOL_PATH" build --configuration:Release --target:Clean "$SOLUTION_PATH"
+# Clean and build the solution for all specified configurations
+configurations=("Debug" "ReleaseWithDebugNativeRef" "Release")
 
-"$VSTOOL_PATH" build --configuration:Debug "$SOLUTION_PATH"
-"$VSTOOL_PATH" build --configuration:ReleaseWithDebugNativeRef "$SOLUTION_PATH"
-"$VSTOOL_PATH" build --configuration:Release "$SOLUTION_PATH"
+for config in "${configurations[@]}"
+do
+    # Clean each configuration
+    dotnet clean "$SOLUTION_PATH" --configuration $config
+
+    # Build each configuration
+    dotnet build "$SOLUTION_PATH" --configuration $config --no-restore
+done
